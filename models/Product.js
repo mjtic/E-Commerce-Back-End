7837f12a67b26cebe64b44518c2139a20858905d
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-06 10:04:16 -0600

    working on setting up models, currently on ProductTag.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // checks for any numbers
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+      }
+    },
   },
   {
     sequelize,
@@ -18,6 +51,7 @@ Product.init(
     underscored: true,
     modelName: "product",
   }
+  
 );
 
 module.exports = Product;
