9265e31ebacf2978d5ed445e60bfead3326310c7
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-06 15:30:56 -0600

    finished models, moving onto api routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
@@ -16,9 +18,17 @@ ProductTag.init(
     product_id:{
       type: DataTypes.INTEGER,
       references:{
-        
+        model: Product,
+        key: "id"
       },
     },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model: Tag,
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
